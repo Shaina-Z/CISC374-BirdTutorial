d4d3a42ed243bcd75990703b33b088e630056019
Shaina <shainaz@udel.edu>
2025-02-23 14:24:45 -0500

    changed

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public GameObject player;
-    public float jumpForce = 10;
+    public float jumpForce = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
