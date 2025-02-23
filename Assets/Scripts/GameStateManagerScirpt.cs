a24da3b0ba974c02cc46ec0666cd99eb0155b48f
Shaina <shainaz@udel.edu>
2025-02-23 15:44:06 -0500

    fixed text trigger

--- Assets/Scripts/GameStateManagerScirpt.cs
+++ Assets/Scripts/GameStateManagerScirpt.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameStateManagerScirpt : MonoBehaviour
 {
-    private int pointsScored=0;
+    public int pointsScored=0;
     public Text scoreText;
+    public GameObject GameOverScreen;
 
 [ContextMenu("Increase Score")]
     public void addPoint(){
         pointsScored++;
         scoreText.text=pointsScored.ToString();
+    }
+
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver(){
+
     }
 }
