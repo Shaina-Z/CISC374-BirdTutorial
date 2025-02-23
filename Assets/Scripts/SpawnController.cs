d4a19ac11fd2c72b10e73c8256c4ab68caf7ac3a
Shaina <shainaz@udel.edu>
2025-02-23 14:24:12 -0500

    added random height to pipe spawn

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -6,6 +6,7 @@ public class SpawnController : MonoBehaviour
     
     private float timer = 0;
     public float spawnTime=2;
+    public float heightOffset =10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,11 +16,14 @@ public class SpawnController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float lowestPoint =transform.position.y - heightOffset;
+        float highestPoint =transform.position.y + heightOffset;
         if(timer<spawnTime){
             timer+=Time.deltaTime;
         }else{
             timer =0.0f;
-            Instantiate(PipePrefab,transform.position,transform.rotation);
+            Instantiate(PipePrefab,new Vector3(transform.position.x,Random.Range(lowestPoint,highestPoint),
+            0),transform.rotation);
         }
         
     }
