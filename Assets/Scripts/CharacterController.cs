3d4df3e0077118fb8ade29ad8ff60e500fb3851f
Shaina <shainaz@udel.edu>
2025-02-23 15:53:53 -0500

    added collision for game over

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,16 +5,18 @@ public class CharacterController : MonoBehaviour
 {
     public GameObject player;
     public float jumpForce = 20;
+    public bool birdIsAlive=true;
+    public GameStateManagerScirpt gameStateManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameStateManager = GameObject.FindGameObjectWithTag("GameStateManager").GetComponent<GameStateManagerScirpt>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true&&birdIsAlive)
         {
             Jump();
         }
@@ -22,5 +24,10 @@ public class CharacterController : MonoBehaviour
         {
             player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * 10;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        gameStateManager.gameOver();
+        birdIsAlive=false;
     }
 }
