19e99ca73bd020fef9506e41566cbce661f2cb28
Shaina <shainaz@udel.edu>
2025-02-25 20:32:09 -0500

    added start screen

--- Assets/Scripts/GameStateManagerScirpt.cs
+++ Assets/Scripts/GameStateManagerScirpt.cs
@@ -7,9 +7,11 @@ public class GameStateManagerScirpt : MonoBehaviour
     public int pointsScored=0;
     public Text scoreText;
     public GameObject GameOverScreen;
+    public GameObject StartScreen;
     AudioSource GameOver_Sound;
     void Start()
     {
+         Time.timeScale = 0;
         GameOver_Sound = GetComponent<AudioSource>();
     }
  
@@ -19,6 +21,10 @@ public class GameStateManagerScirpt : MonoBehaviour
         scoreText.text=pointsScored.ToString();
     }
 
+ public void startGame(){
+    Time.timeScale = 1;
+    StartScreen.SetActive(false);
+    }
     public void restartGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
