de94b6d04c9721e694c879acad11dba69d5e739f
Shaina <shainaz@udel.edu>
2025-02-27 12:15:01 -0500

    fixed clouds

---
+++ Assets/Scripts/CloudContainer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CloudContainer : MonoBehaviour
+{
+    public int deadSpace = -45;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = transform.position + (Vector3.left*10)*Time.deltaTime;
+        if(gameObject.transform.position.x<deadSpace){
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/CloudSpawn.cs
+++ Assets/Scripts/CloudSpawn.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class CloudSpawn : MonoBehaviour
 {
+    public GameObject CloudPrefab;
+    
+    private float timer = 0;
+    public float spawnTime=2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+         Instantiate(CloudPrefab,transform.position,transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if(timer<spawnTime){
+            timer+=Time.deltaTime;
+        }else{
+            timer =0.0f;
+            Instantiate(CloudPrefab,transform.position,transform.rotation);
+        }
     }
 }
